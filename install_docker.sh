56f650db7ed85f2c8f5dea5d057de82dacd3dc80
Lester Reandino <32542078+lhes23@users.noreply.github.com>
2025-06-28 08:26:21 +0800

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -1,52 +1,53 @@
 #!/bin/bash
 
-# Update and upgrade the system
 echo "Updating and upgrading the system..."
-sudo apt-get update && apt-get upgrade -y
+sudo apt-get update && sudo apt-get upgrade -y
 
-# Install dependencies for Docker
 echo "Installing dependencies..."
 sudo apt-get install apt-transport-https ca-certificates curl software-properties-common -y
 
-# Add Docker's official GPG key
 echo "Adding Docker's GPG key..."
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
-# Add Docker repository
 echo "Adding Docker repository..."
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-# Update package database with Docker packages from the newly added repo
 echo "Updating package database with Docker packages..."
 sudo apt-get update
 
-# Install Docker
 echo "Installing Docker..."
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 
-# Enable Docker to start on boot and start Docker service
+echo "Configuring Docker log rotation..."
+sudo mkdir -p /etc/docker
+cat <<EOF | sudo tee /etc/docker/daemon.json
+{
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "10m",
+    "max-file": "3"
+  }
+}
+EOF
+
 echo "Enabling Docker to start on boot..."
 sudo systemctl enable docker
-echo "Starting Docker..."
-sudo systemctl start docker
+sudo systemctl restart docker
 
 sudo usermod -aG docker $USER
+echo "⚠️  Please log out and back in to use Docker without sudo."
 
-# Verify Docker installation
-echo "Verifying Docker installation..."
-docker --version
 
-# Install Docker Compose
 echo "Installing Docker Compose..."
 DOCKER_COMPOSE_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep 'tag_name' | cut -d\" -f4)
 sudo curl -L "https://github.com/docker/compose/releases/download/$DOCKER_COMPOSE_VERSION/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
-# Apply executable permissions to the Docker Compose binary
+echo "Apply executable permissions to the Docker Compose binary"
 sudo chmod +x /usr/local/bin/docker-compose
 
-# Verify Docker Compose installation
-echo "Verifying Docker Compose installation..."
-docker-compose --version
+echo "Verifying Docker and Docker Compose installation..."
+sudo docker --version
+sudo docker-compose --version
 
 echo "Docker and Docker Compose installed successfully."
 
@@ -66,4 +67,3 @@ sudo chmod 600 /swapfile
 sudo mkswap /swapfile
 sudo swapon /swapfile
 echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
-
